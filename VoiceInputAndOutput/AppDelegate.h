4c40da091c4006c02a7d2abfbe3f3448a9c7f455
OwenBla <zhichao18@gmail.com>
2013-10-18 17:33:50 -0400

    Initial Commit

---
+++ VoiceInputAndOutput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VoiceInputAndOutput
+//
+//  Created by OwenBla on 10/18/13.
+//  Copyright (c) 2013 OwenBla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VoiceInputAndOutput/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VoiceInputAndOutput
+//
+//  Created by OwenBla on 10/18/13.
+//  Copyright (c) 2013 OwenBla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
